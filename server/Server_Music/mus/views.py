20b0f4ed3f7cf0bda00cccfae77e25cfe2a98e63
rishshah <rishrocks17@gmail.com>
2016-11-21 21:20:37 +0530

    api modifications

--- server/Server_Music/mus/views.py
+++ server/Server_Music/mus/views.py
@@ -10,25 +10,29 @@ from django.middleware.csrf import get_token
 from django.core import serializers
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.decorators.csrf import csrf_exempt
+import json
 
 @csrf_exempt
 def getPreferenceList(request):
 	if request.method == 'POST':
-		genre = request.POST.get('genre')
-		g = Genre.objects.filter(name=genre)
-		if len(g) != 0:
-			songs = Song.objects.filter(genre=g)
-			x = serializers.serialize('json',songs)
-			return HttpResponse(x)
+		python = json.loads(request.POST)
+		g=Genre.objects.filter(name__in=python)
+		print(g)
+			# toreturn=[]
+			# if len(g) != 0:
+			# 	songs = Song.objects.filter(genre=g)
+			# 	x = serializers.serialize('json',songs)
+			
+		return HttpResponse("Hi")
 	return JsonResponse({'songs':[]})
 
 # @csrf_exempt
 # def recommend(request):
 # 	if request.method == 'POST':
-# 		genre = request.POST.get('genre')
-# 		g = Genre.objects.filter(name=genre)
-# 		if len(g) != 0:
-# 			songs = Song.objects.filter(genre=g)
-# 			x = serializers.serialize('json',songs)
-# 			return HttpResponse(x)
+# 		json = demjson.decode(request.POST)
+# 		for song in json:
+# 			Song.objects.get(name=song) 
+# 				songs = Song.objects.filter(genre=g)
+# 				x = serializers.serialize('json',songs)
+# 				return HttpResponse(x)
 # 	return JsonResponse({'songs':[]})
