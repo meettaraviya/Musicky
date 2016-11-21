422350ba5d4b7c335d6751c234fe6ae2149eda3e
rishshah <rishrocks17@gmail.com>
2016-11-21 22:53:13 +0530

    N6

--- server/Server_Music/mus/models.py
+++ server/Server_Music/mus/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Genre(models.Model):
-	name = models.TextField(max_length=50,,primary_key=True)
+	name = models.TextField(max_length=50,primary_key=True)
 	def __str__(self):
 		return "%s" %(self.name)
 # class Artist(models.Model):
@@ -12,13 +12,13 @@ class Song(models.Model):
 	genre = models.ForeignKey(Genre)
 	rating = models.SmallIntegerField(default=0)
 	artist = models.TextField(max_length=50)
-	name = models.TextField(max_length=50,,primary_key=True)
+	name = models.TextField(max_length=50,primary_key=True)
 
 	def __str__(self):
 		return "%s" %(self.name)
 
 class AppUser(models.Model):
-	username =models.TextField(max_length=50,primary_key=True)
+	username =models.TextField(max_length=50,primary_key=True,default="1")
 	genres = models.ManyToManyField(Genre)
 	songs = models.ManyToManyField(Song)
 	def __str__(self):
