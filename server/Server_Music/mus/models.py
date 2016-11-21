14c2581ee4fbe8d87f5e2d1efe7114396b3ac635
rishshah <rishrocks17@gmail.com>
2016-11-21 20:00:18 +0530

    Some changes

--- server/Server_Music/mus/models.py
+++ server/Server_Music/mus/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Genre(models.Model):
    name = models.TextField(max_length=50) 
@@ -6,6 +7,11 @@ class Genre(models.Model):
 # class Artist(models.Model):
 #    name = models.TextField(max_length=50) 
 
+class AppUser(models.Model)
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	genres = models.ManyToManyField(Genre)
+	songs = models.ManyToManyField(Song)
+	
 class Song(models.Model):
 	genre = models.ForeignKey(Genre)
 	rating = models.SmallIntegerField(default=0)
--- server/Server_Music/mus/views.py
+++ server/Server_Music/mus/views.py
@@ -14,13 +14,21 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def getPreferenceList(request):
 	if request.method == 'POST':
-		print(request.POST)
 		genre = request.POST.get('genre')
-		print(genre)
 		g = Genre.objects.filter(name=genre)
-		print(len(g))
 		if len(g) != 0:
 			songs = Song.objects.filter(genre=g)
 			x = serializers.serialize('json',songs)
 			return HttpResponse(x)
 	return JsonResponse({'songs':[]})
+
+# @csrf_exempt
+# def recommend(request):
+# 	if request.method == 'POST':
+# 		genre = request.POST.get('genre')
+# 		g = Genre.objects.filter(name=genre)
+# 		if len(g) != 0:
+# 			songs = Song.objects.filter(genre=g)
+# 			x = serializers.serialize('json',songs)
+# 			return HttpResponse(x)
+# 	return JsonResponse({'songs':[]})
