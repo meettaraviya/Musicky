fb3c2a1ddf103c8e7bc6b4410cbe4a9c4b6ae7d4
rishshah <rishrocks17@gmail.com>
2016-11-22 02:44:40 +0530

    N15

--- server/Server_Music/mus/views.py
+++ server/Server_Music/mus/views.py
@@ -14,61 +14,61 @@ import json
 
 @csrf_exempt
 def getPreferenceList(request):
-    if request.method == 'POST':
-	    print(request.POST)
-	    userid = request.POST['id']
-	    imp=[]
-	    for i in request.POST:
-	        imp.append(request.POST[i])
-	    a = AppUser(username=userid)
-	    a.save()
-	    print(a)
-	    g=Genre.objects.filter(name__in=imp)
-	    for gen in g:
-	        print(gen)
-	        a.genres.add(gen)
-	    songs = Song.objects.filter(genre__in=g)
-	    print(songs)
-	    x = serializers.serialize('json',songs)
-    return HttpResponse(x)
-    return JsonResponse({	'songs':[]})
+	if request.method == 'POST':
+		print(request.POST)
+		userid = request.POST['id']
+		imp=[]
+		for i in request.POST:
+			imp.append(request.POST[i])
+		a = AppUser(username=userid)
+		a.save()
+		print(a)
+		g=Genre.objects.filter(name__in=imp)
+		for gen in g:
+			print(gen)
+			a.genres.add(gen)
+		songs = Song.objects.filter(genre__in=g)
+		print(songs)
+		x = serializers.serialize('json',songs)
+	return HttpResponse(x)
+	return JsonResponse({	'songs':[]})
 
 @csrf_exempt
 def rate(request):
-    if request.method == 'POST':
-        print(request.POST)
-        u = AppUser.objects.filter(username=request.POST['id'])
-        print(u)
-        if len(u)!=0:
-            user = u[0]
-            print(user)
-            initdict={}
-            for song in request.POST:
-                if song!='id' :
-                    r = Rating(value=int(request.POST[song][0]) ,song_id=song)
-                    print(r)
-                    user.rating_set.add(r)
-                    r.save()
-            		initdict[song] = int(request.POST[song][0]) 
-         
-            allusers = AppUser.objects.all()
-            vect_all=[]
-            for us in allusers:
-            	print(us)
-            	vect_us=0
-            	rates=Rating.objects.filter(user=us)
-            	for s in initdict:
-            		print(s)
-            		r =rates.objects.filter(song_id=s)
+	if request.method == 'POST':
+		print(request.POST)
+		u = AppUser.objects.filter(username=request.POST['id'])
+		print(u)
+		if len(u)!=0:
+			user = u[0]
+			print(user)
+			initdict={}
+			for song in request.POST:
+				if song!='id' :
+					r = Rating(value=int(request.POST[song][0]) ,song_id=song)
+					print(r)
+					user.rating_set.add(r)
+					r.save()
+					initdict[song] = int(request.POST[song][0]) 
+		 
+			allusers = AppUser.objects.all()
+			vect_all=[]
+			for us in allusers:
+				print(us)
+				vect_us=0
+				rates=Rating.objects.filter(user=us)
+				for s in initdict:
+					print(s)
+					r =rates.objects.filter(song_id=s)
 					if len(r) !=0 :
 						print(r)
 						vect_us=vect_us+(initdict[s] - r.value)**2 
 				vect_all.append({us.username:vect_us})
-            print(vect_all)
-            sorted(vect_all, key=lambda x:vect_all[x])
-            print(vect_all)
-            return JsonResponse({'status':'yes'})
-    return JsonResponse({'status':'no'})
+			print(vect_all)
+			sorted(vect_all, key=lambda x:vect_all[x])
+			print(vect_all)
+			return JsonResponse({'status':'yes'})
+	return JsonResponse({'status':'no'})
 
 
 @csrf_exempt
