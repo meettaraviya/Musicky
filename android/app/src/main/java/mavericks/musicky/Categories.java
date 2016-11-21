db13111e005ef7e7e38c739d04a76699adb4574f
Meet <meet11061997@gmail.com>
2016-11-21 12:48:54 +0530

    init

---
+++ android/app/src/main/java/mavericks/musicky/Categories.java
@@ -0,0 +1,13 @@
+package mavericks.musicky;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Categories extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.categories);
+    }
+}
