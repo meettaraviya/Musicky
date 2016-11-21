075a0a5c22f2b758b8366ed1fae2c77ac3e4d373
RISHABH-JARVIS\Rishabh <rishabhga97@gmail.com>
2016-11-21 12:53:52 +0530

    'Base Youtube

---
+++ android/app/src/main/java/mavericks/musicky/MusicActivity.java
@@ -0,0 +1,66 @@
+package mavericks.musicky;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerFragment;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MusicActivity extends YouTubeBaseActivity
+        implements YouTubePlayer.OnInitializedListener {
+
+
+    public static final String DEVELOPER_KEY="AIzaSyAI5YtqVjFmlp-2Y4r4gPM4wZ2DAQDwL5M";
+    public static final String VIDEO_ID="srH-2pQdKhg";
+    public static final int RECOVERY_DIALOG_REQUEST=1;
+
+    YouTubePlayerFragment myYouTubePlayerFragment;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music);
+        myYouTubePlayerFragment = (YouTubePlayerFragment)getFragmentManager()
+                .findFragmentById(R.id.youtubeplayerfragment);
+        myYouTubePlayerFragment.initialize(DEVELOPER_KEY, this);
+    }
+
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                        YouTubeInitializationResult errorReason) {
+        if (errorReason.isUserRecoverableError()) {
+            errorReason.getErrorDialog(this, RECOVERY_DIALOG_REQUEST).show();
+        } else {
+            String errorMessage = String.format(
+                    "There was an error initializing the YouTubePlayer (%1$s)",
+                    errorReason.toString());
+            Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+        }
+    }
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player,
+                                        boolean wasRestored) {
+        if (!wasRestored) {
+            player.cueVideo(VIDEO_ID);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_DIALOG_REQUEST) {
+// Retry initialization if user performed a recovery action
+            getYouTubePlayerProvider().initialize(DEVELOPER_KEY, this);
+        }
+    }
+    protected YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return (YouTubePlayerView)findViewById(R.id.youtubeplayerfragment);
+    }
+}
+
